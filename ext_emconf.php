d20ce081eadaebcd9092eeb00e0fa7d14231d98f
Marc Bastian Heinrichs <typo3@mbh-software.de>
2023-03-22 12:50:15 +0100

    [TASK] Add composer.json and ext_emconf.php

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF['treehide'] = [
+    'title' => 'Hide / unhide pages recursive',
+    'description' => '',
+    'category' => '',
+    'author' => 'Marc Bastian Heinrichs',
+    'author_email' => 'mbh@mbh-software.de',
+    'state' => 'alpha',
+    'author_company' => '',
+    'version' => '1.0.0-dev',
+    'constraints' => [
+        'depends' => [
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
