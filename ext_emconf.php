5e6affdab792d7089705b8e702d24713b9eda063
Sybille Peters <sypets@gmx.de>
2024-10-26 09:23:12 +0200

    [FEATURE] Make ready for v12
    - Replace TYPO3_MODE
    - use automatic registration for v12 with Services.yaml
    - load JavaScript with JavaScriptModules
    - use ES6 instead of requirejs in JavaScript
    
    Resolves: #3

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF['treehide'] = [
     'clearCacheOnLoad' => true,
     'version' => '0.10.0',
     'constraints' => [
-        'depends' => ['typo3' => '10.4.0-11.5.99'],
+        'depends' => ['typo3' => '10.4.0-12.4.99'],
         'conflicts' => [],
         'suggests' => [],
     ],
